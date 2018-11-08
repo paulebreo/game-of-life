0a29298aac1171ac92272c8cef64cdb277b92518
Paul Ebreo <paulebreo@triton.edu>
2018-11-08 15:22:15 -0600

    render dummy table data

--- ExploreCalifornia/wwwroot/client/components/Main.js
+++ ExploreCalifornia/wwwroot/client/components/Main.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ControlPanel from './ControlPanel'
 import Table from './Table'
 
+const DUMMY_DATA = [[0, 1, 0], [1, 0, 1]]
 const Main = props => (
   <React.Fragment>
     <h1>helloGame of Life</h1>
-    <Table />
+    <Table data={DUMMY_DATA} />
     <ControlPanel />
   </React.Fragment>
 )
--- ExploreCalifornia/wwwroot/client/components/Table.js
+++ ExploreCalifornia/wwwroot/client/components/Table.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import uuidv1 from 'uuid/v1'
 
 const Table = props => (
   <React.Fragment>
-    <table id="board" />
+    <table id="board">
+      <tbody>
+        {props.data.map(row => (
+          <tr key={uuidv1()}>
+            {row.map(col => (
+              <td key={uuidv1()}>{col}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </React.Fragment>
 )
 
