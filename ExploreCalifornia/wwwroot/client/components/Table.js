350149fa97567fb706708cadcc4bbebe2213717f
Paul Ebreo <paulebreo@triton.edu>
2018-11-12 04:02:12 -0600

    refactor table component to class

--- ExploreCalifornia/wwwroot/client/components/Table.js
+++ ExploreCalifornia/wwwroot/client/components/Table.js
@@ -4,31 +4,35 @@ import { connect } from 'react-redux'
 import { mouseDown, mouseUp } from '../store'
 import TableCell from './TableCell'
 
-const Table = props => (
-  <React.Fragment>
-    <table
-      id="board"
-      onMouseDown={props.handleMouseDown}
-      onMouseUp={props.handleMouseUp}
-    >
-      <tbody>
-        {props.tableData.map((row, rowIdx) => (
-          <tr key={uuidv1()}>
-            {row.map((col, colIdx) => (
-              // <td className={col === true ? 'alive' : ''} key={uuidv1()} />
-              <TableCell
-                className={col === true ? 'alive' : ''}
-                key={uuidv1()}
-                rowIdx={rowIdx}
-                colIdx={colIdx}
-              />
+class Table extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <table
+          id="board"
+          onMouseDown={this.props.handleMouseDown}
+          onMouseUp={this.props.handleMouseUp}
+        >
+          <tbody>
+            {this.props.tableData.map((row, rowIdx) => (
+              <tr key={uuidv1()}>
+                {row.map((col, colIdx) => (
+                  // <td className={col === true ? 'alive' : ''} key={uuidv1()} />
+                  <TableCell
+                    className={col === true ? 'alive' : ''}
+                    key={uuidv1()}
+                    rowIdx={rowIdx}
+                    colIdx={colIdx}
+                  />
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </React.Fragment>
-)
+          </tbody>
+        </table>
+      </React.Fragment>
+    )
+  }
+}
 
 const mapState = state => {
   const { tableData } = state
