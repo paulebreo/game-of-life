d23bc9aefe54902dd5c3dd1af5ad60ffaae58365
Paul Ebreo <paulebreo@triton.edu>
2018-11-12 07:12:03 -0600

    clean up: remove commented code

--- ExploreCalifornia/wwwroot/client/components/TableCell.js
+++ ExploreCalifornia/wwwroot/client/components/TableCell.js
@@ -2,29 +2,6 @@ import React, { Component } from 'react'
 import store, { toggleCell } from '../store'
 import { connect } from 'react-redux'
 
-// const TableCell = props => (
-//   <td
-//     className={props.className}
-//     onClick={props.handleToggleCell}
-//     onMouseOver={props.handleMouseOver}
-//   />
-// )
-
-// const mapDispatch = (dispatch, ownProps) => {
-//   return {
-//     handleToggleCell: evt => {
-//       evt.preventDefault()
-//       dispatch(toggleCell(ownProps.rowIdx, ownProps.colIdx))
-//     },
-//     handleMouseOver: evt => {
-//       evt.preventDefault()
-//       if (store.getState().isMouseDown) {
-//         store.dispatch(toggleCell(ownProps.rowIdx, ownProps.colIdx))
-//       }
-//     }
-//   }
-// }
-
 export class TableCell extends Component {
   constructor(props) {
     super(props)
@@ -54,7 +31,3 @@ export class TableCell extends Component {
 }
 
 export default TableCell
-// export default connect(
-//   null,
-//   mapDispatch
-// )(TableCell)
